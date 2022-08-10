0f10fc9777b35017b461e3113a113ac5c4441bda
rabia <rabia.olker@gmail.com>
2022-08-11 01:00:48 +0300

    main build of redux created

---
+++ src/redux/covidSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const covidSlice = createSlice({
+  name: 'covid',
+  initialState: {},
+  reducers: {}
+})
+
+// Action creators are generated for each case reducer function
+export const {} = covidSlice.actions
+
+export default covidSlice.reducer
---
+++ src/redux/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit'
+import covidReducer from "./covidSlice"
+
+export default configureStore({
+  reducer: {
+    covid:covidReducer
+  }
+})
