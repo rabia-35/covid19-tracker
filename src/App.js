cc8746e760be2850fffb79908906a2e039058c9e
rabia <rabia.olker@gmail.com>
2022-08-13 00:46:06 +0300

    exported graphic file to appjs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Cards from './components/cards';
+import Graphic from './components/graphic';
 import Header from './components/header';
 
 
@@ -7,7 +8,8 @@ function App() {
   return (
     <div className="App">
       <Header />
-        <Cards />
+      <Cards />
+      <Graphic />
     </div>
   );
 }
