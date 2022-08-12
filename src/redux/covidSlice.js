b73bf465dbfae48b03cff4fa0c5a95f102f18789
rabia <rabia.olker@gmail.com>
2022-08-12 19:07:40 +0300

    added parametre (country) to fetchCovid. Added selectedCountry to reducers for country name

--- src/redux/covidSlice.js
+++ src/redux/covidSlice.js
@@ -1,15 +1,17 @@
 import {createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from 'axios'
 
-export const fetchCovid=createAsyncThunk("covid", async ()=>{
-  const res= await axios(`${process.env.REACT_APP_API_BASE_ENDPOINT}/`)
+export const fetchCovid=createAsyncThunk("covid", async (country)=>{
+  const res= await axios(`${process.env.REACT_APP_API_BASE_ENDPOINT}/${country ? `countries/${country}` : "" }`)
   return res.data
 }) 
 
-export const fetchCountryCovid=createAsyncThunk("covid/country", async ()=>{
+export const fetchCountriesCovid=createAsyncThunk("covid/country", async ()=>{
   const res= await axios(`${process.env.REACT_APP_API_BASE_ENDPOINT}/countries`)
   return res.data
 }) 
+
+
 
 
 export const covidSlice = createSlice({
@@ -18,37 +20,47 @@ export const covidSlice = createSlice({
     items:"",
     status:"idle",
     error:"",
+    countries:null,
     country:null,
   },
-  reducers: {},
+  reducers: {
+    selectedCountry:(state, action)=>{
+      state.country=action.payload
+    }
+  },
   extraReducers:{
+// The area where all data is taken - pending, fullfilled and rejected
     [fetchCovid.pending]: (state) => {
       state.status = 'loading';
-  },
-  [fetchCovid.fulfilled]: (state, action) => {
-      state.items=action.payload
-      state.status = "successed";
     },
-  [fetchCovid.rejected]: (state, action) => {
-      state.status = "failed";
-      state.error = action.error.message;
-  },
-  [fetchCountryCovid.pending]: (state) => {
-    state.status = 'loading';
-},
-[fetchCountryCovid.fulfilled]: (state, action) => {
-    state.country=action.payload
-    state.status = "successed";
-  },
-[fetchCountryCovid.rejected]: (state, action) => {
-    state.status = "failed";
-    state.error = action.error.message;
-},
+    [fetchCovid.fulfilled]: (state, action) => {
+        state.items=action.payload
+        state.status = "successed";
+    },
+    [fetchCovid.rejected]: (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+    },
+
+
+// the area where all countries are taken - pending, fullfilled and rejected
+    [fetchCountriesCovid.pending]: (state) => {
+      state.status = 'loading';
+    },
+    [fetchCountriesCovid.fulfilled]: (state, action) => {
+        state.countries=action.payload
+        state.status = "successed";
+      },
+    [fetchCountriesCovid.rejected]: (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+    },
+
   },
  
 })
 
 // Action creators are generated for each case reducer function
-//export const {} = covidCardSlice.actions
+export const {selectedCountry} = covidSlice.actions
 
 export default covidSlice.reducer
