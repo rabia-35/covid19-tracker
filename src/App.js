261d996b0cb279027c6fcb41fbbd013e710ba7f8
rabia <rabia.olker@gmail.com>
2022-08-12 17:04:14 +0300

    exported files to appjs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Cards from './components/cards';
+import Header from './components/header';
 
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
+        <Cards />
     </div>
   );
 }
